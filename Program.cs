050884d4032e768201e742cb64a7e2c354cca493
shohangithub <ris.shohan@gmail.com>
2023-02-03 23:02:52 +0600

    Initial azure app configuration connection

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder();
+builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("AzureAppConfig"));
+
+var config = builder.Build();
+Console.WriteLine(config["AzureAppConfiguration:ConnectionString:SQL"] ?? "Hello world!");
